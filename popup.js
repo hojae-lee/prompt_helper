ea36dc5032dbe88b807893185952f92348d3509c
jary-lee <jary.j.lee@polarisshare.com>
2023-04-20 15:31:33 +0900

    update : 프롬프트 템플릿 추가

--- popup.js
+++ popup.js
@@ -1,15 +1,43 @@
-let job = "Marketer";
+const W = window;
+W.JOB = "Marketer";
 
 document.addEventListener("DOMContentLoaded", () => {
   function inputValue(main, sub) {
+    const marketerData = `마케팅 : 설문조사
+      Act as a survey creator. you need to create survey questions about the topic of the bottom.
+      You need to create 7 questions that people will be interested in answering. Also, create a choice. Create a mix of multiple-choice questions with 5 or so options to choose from, and open-ended questions for short answers. Some multiple choice questions should have three options, some should have four, and the essay answers should have a short explanation.
+
+      The topic is`;
+    const develpData = `개발 : 소프트웨어 디자인
+      Software design prompt
+      You are tasked with designing a new software application. Please provide a brief description of the application's purpose and functionality, and ask the AI language model to provide suggestions for the software architecture, design patterns, and technologies to use.
+
+      The application topic is`;
+    const designData = `디자인 : 지속 가능한 제품 디자인
+      You are a product designer tasked with creating a sustainable product. I'll put the product category at the very bottom. Design a product that is environmentally friendly, socially responsible, and economically viable. Your design should include the following:
+
+      - Detailed specifications of the product, including materials, manufacturing process, and end-of-life disposal.
+      - An analysis of the environmental impact of the product, including its carbon footprint and other ecological impacts.
+      - A marketing strategy that emphasizes the product's sustainability features and benefits to consumers.
+
+      The product category is`;
+    let result = "";
+    switch (main) {
+      case "Marketer":
+        result = marketerData;
+        break;
+      case "Designer":
+        result = designData;
+        break;
+      default:
+        result = develpData;
+    }
     const textarea = document.querySelector("textarea");
-    textarea.value = main + sub;
-    const submit = textarea.nextElementSibling;
-    submit.click();
+    textarea.value = result + " " + sub;
   }
 
   document.querySelector("#jobSelect").addEventListener("change", (e) => {
-    job = e.target.value;
+    W.JOB = e.target.value;
   });
 
   document.querySelector("#applyBtn").addEventListener("click", async () => {
@@ -19,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
     await chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: inputValue,
-      args: [job, inputStr],
+      args: [W.JOB, inputStr],
     });
   });
 });
