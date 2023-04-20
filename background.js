ee4342cb76366885eb42a2ff04e40b4fe8b6b69e
jary-lee <jary.j.lee@polarisshare.com>
2023-04-20 15:31:03 +0900

    update : 특정 도메인에서 동작하도록 추가

--- background.js
+++ background.js
@@ -1,10 +1,27 @@
-function msg() {
-  alert("hello, world");
-}
+// function msg() {
+//   alert("hello, world");
+// }
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: msg,
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: msg,
+//   });
+// });
+
+// 특정 도메인을 제외하고는 사용되지 않도록 합니다.
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.action.disable();
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostSuffix: "openai.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
   });
 });
